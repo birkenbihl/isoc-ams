853be924dd6a3251aa3199807b8506d3c37c27db
birkenbihl <klaus@ict-media.de>
2026-02-09 17:44:26 +0100

    typo correction

--- isoc_de_ams_main.py
+++ isoc_de_ams_main.py
@@ -81,7 +81,7 @@ def main(dryrun, headless, amsmail):    # dryrun will only build the lists but w
                                # headless controls weather the browser window will be opened
     global ams
 
-    ams = ISOC_AMS(process_pendings
+    ams = ISOC_AMS(
                    *isoc_de.ams_credentials,
                    headless=headless,
                    logfile=sys.stdout,
